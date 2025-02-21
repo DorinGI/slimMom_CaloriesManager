cd638893505f9af93b79821d05e435e5fc391e26
DorinGI <158717197+DorinGI@users.noreply.github.com>
2025-02-21 19:18:42 +0100

    CSS

--- frontend_react/src/pages/HomePage.jsx
+++ frontend_react/src/pages/HomePage.jsx
@@ -5,6 +5,9 @@ import DailyCaloriesForm from '../components/DailyCaloriesForm/DailyCaloriesForm
 import Loader from '../components/Loader/Loader';
 import Modal from '../components/Modal/Modal';
 import styles from './HomePage.module.css';
+import Banana from '../../images/Banana.png';
+import Strawberry from '../../images/Strawberry.png';
+import Leaf from '../../images/Leaf.png';
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -26,22 +29,44 @@ const Home = () => {
 
   return (
     <div className={styles.homeContainer}>
-      <p className={styles.description}>
-        Calculează necesarul zilnic de calorii pentru un stil de viață sănătos.
-      </p>
+      <div className={styles.formContainer}>
+        <p className={styles.description}>
+          Calculate your daily calorie intake right now
+        </p>
 
-      <DailyCaloriesForm onSubmit={handleFormSubmit} />
+        <DailyCaloriesForm onSubmit={handleFormSubmit} />
 
-      {loading && <Loader />}
-      {error && <p className={styles.error}>Eroare: {error}</p>}
+        {loading && <Loader />}
+        {error && <p className={styles.error}>Eroare: {error}</p>}
 
-      {kcal && modalOpen && (
-        <Modal
-          onClose={() => setModalOpen(false)}
-          products={restrictedProducts || []}
-          kcal={kcal}
-        />
-      )}
+        {kcal && modalOpen && (
+          <Modal
+            onClose={() => setModalOpen(false)}
+            products={restrictedProducts || []}
+            kcal={kcal}
+          />
+        )}
+      </div>
+      <div className={styles.graphics}>
+        <img src={Banana} alt="Banana" className={styles.banana} />
+        <img src={Strawberry} alt="Strawberry" className={styles.strawberry} />
+        <img src={Leaf} alt="Leaf" className={styles.leaf} />
+
+        {/* SVG în colțul dreapta jos */}
+        <svg
+          className={styles.svgShape}
+          width="603"
+          height="816"
+          viewBox="0 0 603 816"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M206 259C51.6 271.8 6 403.667 1.5 468C-8.1 648.4 119.167 775.167 185 816H603V-0.000244141C603 -0.000244141 574.5 24.4998 570 68.9997C533 262 534.5 291 467.5 300.5C400.5 310 316 249.881 206 259Z"
+            fill="#F0F1F3"
+          />
+        </svg>
+      </div>
     </div>
   );
 };
